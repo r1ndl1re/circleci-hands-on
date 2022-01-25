6fc0d1f4241f668bb85dabff6a68255b83ee6f50
Rendlire <rendlire@gmail.com>
2022-01-25 09:10:32 +0000

    refs #6 add-region-settings

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,7 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.72.0"
+      version = "~> 3.7"
     }
   }
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
 }
