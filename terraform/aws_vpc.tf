caa2b2040e12e47188c0841851d4874865e6a998
Rendlire <rendlire@gmail.com>
2022-01-27 11:24:53 +0000

    refs #9 add route tables

--- terraform/aws_vpc.tf
+++ terraform/aws_vpc.tf
@@ -18,3 +18,13 @@ resource "aws_subnet" "public" {
 resource "aws_internet_gateway" "example" {
   vpc_id = aws_vpc.example.id
 }
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.example.id
+}
+
+resource "aws_route" "public" {
+  route_table_id         = aws_route_table.public.id
+  gateway_id             = aws_internet_gateway.example.id
+  destination_cidr_block = "0.0.0.0/0"
+}
