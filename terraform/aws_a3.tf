e16ed35f9ff8eb0279bf34a786071c662982600c
Rendlire <rendlire@gmail.com>
2022-01-27 09:56:29 +0000

    refs #9 add vpc settings

--- terraform/aws_a3.tf
+++ terraform/aws_a3.tf
@@ -19,7 +19,7 @@ resource "aws_s3_bucket" "private" {
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "pricate" {
+resource "aws_s3_bucket_public_access_block" "private" {
   bucket                  = aws_s3_bucket.private.id
   block_public_acls       = true
   block_public_policy     = true
