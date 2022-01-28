76a0d738a385340b146ce23feda96098cee464c9
Rendlire <rendlire@gmail.com>
2022-01-28 04:19:31 +0000

    refs #9 add public and private subnets

--- terraform/aws_vpc.tf
+++ terraform/aws_vpc.tf
@@ -28,3 +28,24 @@ resource "aws_route" "public" {
   gateway_id             = aws_internet_gateway.example.id
   destination_cidr_block = "0.0.0.0/0"
 }
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_subnet" "private" {
+  vpc_id                  = aws_vpc.example.id
+  cidr_block              = "10.0.64.0/24"
+  availability_zone       = "ap-northeast-1a"
+  map_public_ip_on_launch = false
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.example.id
+}
+
+resource "aws_route_table_association" "private" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.private.id
+}
